b306f0866ca6b93a98b769d3396cfc888eff38d3
thoriumsnake <thoriumsnake@outlook.com>
2022-02-15 11:53:08 -0300

    Call movement functions from FixedUpdate

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     Movement2DController controller;
+    float horizontalMovement;
+    bool jump;
+
     void Start() {
         controller = gameObject.GetComponent<Movement2DController>();
     }
 
     // Update is called once per frame
     void Update() {
-        controller.Move(Input.GetAxisRaw("Horizontal"));
-        controller.Jump(Input.GetButtonDown("Jump"));
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        //Update is called more than FixedUpdate, so the jump variable can't be dependent on whether the user pressed jump on a specific frame
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+
+        //Debug.Log(gameObject.GetComponent<Rigidbody2D>().velocity);
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMovement);
+        controller.Jump(ref jump);
+        //Debug.Log("jump: " + jump);
     }
 }
